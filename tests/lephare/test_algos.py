310db88b4c1651f2e772011ad334d64e222a71b4
Drew Oldag <awoldag@uw.edu>
2024-04-26 07:46:55 -0700

    Including one assert in the inform_stage unit test.

--- tests/lephare/test_algos.py
+++ tests/lephare/test_algos.py
@@ -1,8 +1,8 @@
-from rail.estimation.algos.lephare import LephareInformer, LephareEstimator
 import numpy as np
+from rail.estimation.algos.lephare import LephareInformer
 
 
-def test_basic_estimator_stage_creation():
+def test_basic_informer_stage_creation():
     inform_lephare = LephareInformer.make_stage(
         name="inform_Lephare",
         nondetect_val=np.nan,
@@ -10,13 +10,18 @@ def test_basic_estimator_stage_creation():
         hdf5_groupname="",
     )
 
+    assert inform_lephare.name == "LephareInformer"
+
     # We need to add some testing data. From the main RAIL repo?
     # inform_lephare.inform(train_data)
 
-    # estimate_lephare = LephareEstimator.make_stage(
-    #     name="test_Lephare",
-    #     nondetect_val=np.nan,
-    #     model=inform_lephare.get_handle("model"),
-    #     hdf5_groupname="",
-    #     aliases=dict(input="test_data", output="lephare_estim"),
-    # )
+# def test_basic_estimator_stage_creation():
+#     estimate_lephare = LephareEstimator.make_stage(
+#         name="test_Lephare",
+#         nondetect_val=np.nan,
+#         model=inform_lephare.get_handle("model"),
+#         hdf5_groupname="",
+#         aliases=dict(input="test_data", output="lephare_estim"),
+#     )
+
+#     assert estimate_lephare.name == "LephareEstimator"
